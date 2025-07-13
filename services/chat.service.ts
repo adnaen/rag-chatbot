76fd3cfbf599d6a0a856acb3d38c0493f6fcd752
Adnan <adnanmuhammedadnancp@gmail.com>
2025-07-13 19:41:54 +0530

    add api service in next.js

---
+++ services/chat.service.ts
@@ -0,0 +1,43 @@
+import { TChat, TChatCreate } from "@/types/common";
+
+export class ChatService {
+  static readonly API_URL = process.env.NEXT_PUBLIC_API_URL
+
+  static async getAllChats(): Promise<TChat[] | null> {
+    try {
+      const response = await fetch(`${this.API_URL}/chats`)
+      if (response.ok) {
+        const data: TChat[] = await response.json()
+        return data;
+      }
+      throw Error(`Error occur in fetch all chats as '${response.status}'`)
+    } catch (err) {
+      console.error(err)
+      return null;
+    }
+  }
+
+  static async addChat(newChat: TChatCreate): Promise<TChat | null> {
+    try {
+      const response = await fetch(`${this.API_URL}/chat`, {
+        method: "POST",
+        body: JSON.stringify(newChat),
+        headers: {
+          "Content-Type": "applications/json"
+        },
+        credentials: "include",
+      })
+
+      if (response.ok) {
+        const data: TChat = await response.json()
+        return data;
+      }
+      throw Error(`Error occur in fetch all chats as '${response.status}'`)
+
+    } catch (err) {
+      console.error(err)
+      return null;
+    }
+
+  }
+}
--- types/common.d.ts
+++ types/common.d.ts
@@ -1,5 +1,9 @@
 export type TChat = {
+  id: string,
   prompt: string | null,
   assistant?: string | null,
+  created_at: Date,
 }
+
+export type TChatCreate = Omit<TChat, 'id' | 'created_at'>
 
