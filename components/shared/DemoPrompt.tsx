38b87bcfe566a60f791e1237c158b64e3c9c2eaf
Adnan <adnanmuhammedadnancp@gmail.com>
2025-07-14 10:51:48 +0530

    update demo prompt style

--- components/shared/DemoPrompt.tsx
+++ components/shared/DemoPrompt.tsx
@@ -1,7 +1,17 @@
+import { Lightbulb } from "lucide-react";
+
 const DemoPrompt = ({ prompt }: { prompt: string }) => {
 	return (
-		<div className="bg-white/10 border border-white/30 text-white p-3 rounded-lg">{prompt}</div>
+		<div className="bg-white/10 border hover:cursor-pointer hover:bg-white/20 hover:border-white/80 border-transparent text-white p-3 rounded-lg transition-colors flex">
+			<div className="mr-2">
+				<Lightbulb />
+			</div>
+			<div className="text-start">
+				{prompt}
+			</div>
+		</div>
 	)
 }
 
 export default DemoPrompt;
+
